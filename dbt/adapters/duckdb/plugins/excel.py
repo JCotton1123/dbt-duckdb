313aca15011e6840006e7bec1a391489c2f5442c
Jesse Cotton <JCotton1123@gmail.com>
2023-09-29 10:55:39 -0700

    Fix formatting

--- dbt/adapters/duckdb/plugins/excel.py
+++ dbt/adapters/duckdb/plugins/excel.py
@@ -10,8 +10,8 @@ from . import BasePlugin
 from . import pd_utils
 from ..utils import SourceConfig
 from ..utils import TargetConfig
-
 from dbt.logger import GLOBAL_LOGGER as logger
+
 
 class Plugin(BasePlugin):
     def initialize(self, plugin_config: Dict[str, Any]):
@@ -29,7 +29,6 @@ class Plugin(BasePlugin):
         if "s3_region" in plugin_config:
             os.environ["AWS_DEFAULT_REGION"] = plugin_config["s3_region"]
 
-
     def load(self, source_config: SourceConfig):
         ext_location = source_config["external_location"]
         ext_location = ext_location.format(**source_config.as_dict())
@@ -41,7 +40,6 @@ class Plugin(BasePlugin):
         sheet_name = source_config.get("sheet_name", 0)
         return pd.read_excel(source_location, sheet_name=sheet_name)
 
-
     def store(self, target_config: TargetConfig):
         plugin_output_config = self._config["output"]
 
@@ -49,20 +47,24 @@ class Plugin(BasePlugin):
         # Instead if we instantiated the writer in the constructor
         # with mode = 'w', this would result in an existing file getting
         # overwritten. This can happen if dbt test is executed for example.
-        if not hasattr(self, '_excel_writer'):
+        if not hasattr(self, "_excel_writer"):
             self._excel_writer = pd.ExcelWriter(
                 plugin_output_config["file"],
                 mode=plugin_output_config.get("mode", "w"),
                 engine=plugin_output_config.get("engine", "xlsxwriter"),
                 engine_kwargs=plugin_output_config.get("engine_kwargs", {}),
                 date_format=plugin_output_config.get("date_format"),
-                datetime_format=plugin_output_config.get("datetime_format")
+                datetime_format=plugin_output_config.get("datetime_format"),
             )
-            if "header_styling" in plugin_output_config and plugin_output_config["header_styling"] == False:
+            if not plugin_output_config.get("header_styling", True):
                 excel.ExcelFormatter.header_style = None
 
-        target_output_config = {**plugin_output_config, **target_config.config.get("overrides", {})}
-        if not "sheet_name" in target_output_config:
+        target_output_config = {
+            **plugin_output_config,
+            **target_config.config.get("overrides", {})
+        }
+
+        if "sheet_name" not in target_output_config:
             target_output_config["sheet_name"] = target_config.relation.identifier
 
         df = pd_utils.target_to_df(target_config)
@@ -74,11 +76,10 @@ class Plugin(BasePlugin):
             header=target_output_config.get("header", True),
             index=target_output_config.get("index", True),
             merge_cells=target_output_config.get("merge_cells", True),
-            inf_rep=target_output_config.get("inf_rep", "inf")
+            inf_rep=target_output_config.get("inf_rep", "inf"),
         )
 
-
     def __del__(self):
-        if hasattr(self, '_excel_writer'):
+        if hasattr(self, "_excel_writer"):
             logger.info(f"Closing {self._config['output']['file']}")
             self._excel_writer.close()
